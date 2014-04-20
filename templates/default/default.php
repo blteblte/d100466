d17c277fcf5453d56606ce074dac7d9fe79ca387
blteblte <martin.sky@inbox.lv>
2014-04-20 12:48:57 +0300

    ver 0.8
    Added documentation to source code

--- templates/default/default.php
+++ templates/default/default.php
@@ -1,4 +1,22 @@
-
+<?php
+// en: Default template. Uses instance of preloaded modules and is cabaple of calling
+// public functions of it. This template can be called either using static way via index 
+// or asynchronous calls via phpAJAX core saving the needed instance of the module.
+// lv: Noklusējuma šablons. Izmanto iepriekšielādētā moduļa instanci un ir spējīgs
+// izsaukt publiskās moduļa funkcijas. Šo šablonu var izsaukt gan statiskā veidā caur
+// index, gan asinhroni caur phpAJAX core saglabājot moduļu klases instanci.
+//
+// en: Example of calling module public function:
+// lv: Piemērs moduļa statiskās funkcijas izsaukšanai:
+//
+//	en: $load - predefined instance of module
+//	lv: $load - iepriekšdefinēta moduļa instance
+//
+//	$load->functionname();
+//
+// en: Also you can call here any static functions as you desire.
+// lv: Tāpat protams iespējams izsaukt jebkuru statisku funkciju pēc nepieciešamības.
+?>
 
 <!DOCTYPE html>
 <html>
@@ -50,7 +68,21 @@
             </footer>
             <div class="clearfix"></div>
         </div>
-		
+		<?php
+		// en: Required script for phpAJAX navigation. You can use use either AJAX function to load
+		// modules or static url request to index.
+		// lv: Skripts nepieciešams phpAJAX navigācijai. Moduļus var izsaukt gan lietojot AJAX funkciju
+		// gan statisku url pieprasījumu us index
+		//
+		//	en: AJAX navigation example:
+		//	lv: AJAX navigācijas piemērs:
+		//
+		//	en: ModuleName - Name of module you want to call: CaseSensitive!
+		//	en: PageTitle - The title of the page you want to be displayed for current module
+		//	lv: ModuleName - Moduļa nosaukums ko nepieciešams ielādēt. Reģistrjūtīgs!
+		//	lv: PageTitle - Lapas nosaukums ko nepieciešams attēlot ielādējot moduli
+		//	ajaxRequestCore('ModuleName','PageTitle')
+		?>
 		<script src="<?=Site::base_url()?>js/main.js"></script>
 		
     </body>
