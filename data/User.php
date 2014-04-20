2659260506734bb969e51ab12881acf831f33c36
blteblte <martin.sky@inbox.lv>
2014-04-20 14:33:11 +0300

    ver 0.8
    Added documentation to source code

--- data/User.php
+++ data/User.php
@@ -1,11 +1,26 @@
 <?php
+/**
+* phpAJAX simple framework for agile AJAX development
+* 
+* @author Mārtiņš Bitenieks
+* @licence http://www.opensource.org/licenses/mit-license.html MIT License
+* @version Look at /index.php
+*/
+
+// en: DataLayer creating DB object using information from DB table. With use for single PK tables.
+// lv: Datu slānis DB objektu veidošanai izmantojot informāciju par DB tabulu. Lieto vienas PK ietvaros.
 
 class User
 {
     protected $con;
     public function db() {return $this->con;}
     
-    //column names
+    /**
+    * en: Define table column names here. DO NOT DEFINE AUTO_INCREMENT KEY HERE!
+    * lv: Definēt tabulas kolonnas nosaukumus šeit. NENORĀDĪT AUTO_INCREMENT KOLONNU!
+    * 
+    * @public param 	array     @data     column names for current object
+    */
     public $data = array(
         'user_type', 'username', 'nickname',
         'password', 'email', 'reg_dt', 'phone', 'address',
@@ -13,9 +28,16 @@ class User
         'session_token', 'active_ip', 'admin_description'
     );
     
+    // en: AUTO_INCREMENT PK column name here
+    // lv: AUTO_INCREMENT PK kolonnas nosaukums šeit
     protected $pk = 'user_id';
     
-    //columns
+    /**
+    * en: Define all columns as public parameters
+    * lv: Definēt visus tabulas kolonnas kā publiskos objekta parametrus
+    * 
+    * @public param
+    */
     public      $user_id,
                 $user_type,
                 $username,
@@ -33,11 +55,19 @@ class User
                 $active_ip,
                 $admin_description;
     
+    // en: Get connection instance for object. Use with new Object(@con)
+    // lv: Saņemt DB pieslēguma instanci objektam. Lietot ar new Object(@con)
     public function __construct($con)
     {
         $this->con = $con;
     }
     
+    /**
+    * en: Get count for columns which are left as null for generating query script
+    * lv: Saņemt objekta null parametru skaitu SQL skriptu ģenerēšanai
+    * 
+    * @return	int     @count	count of null columns
+    */
     public function getNullColumnsCount(){
         $count = 0;
         foreach($this->data as $col)
@@ -47,6 +77,12 @@ class User
         return ($count + 1);
     }
     
+    /**
+    * en: Generate insert script and executed prepared PDO statement
+    * lv: Ģenerē insert skriptu in izpilda sagatavoto PDO pieprasījumu
+    * 
+    * @return	int     ? 0 : 1
+    */
     public function Insert(){
         $nullColumns = $this->getNullColumnsCount();
         $table = get_class($this) . 's';
@@ -84,6 +120,12 @@ class User
         return 1;
     }
     
+    /**
+    * en: Generate update script and executed prepared PDO statement
+    * lv: Ģenerē update skriptu in izpilda sagatavoto PDO pieprasījumu
+    * 
+    * @return	int     ? 0 : 1
+    */
     public function Update(){
         $nullColumns = $this->getNullColumnsCount();
         $table = get_class($this) . 's';
@@ -110,6 +152,12 @@ class User
         return 1;
     }
     
+    /**
+    * en: Get all teble data and return object collection
+    * lv: Saņem visus tabulas datus un atgriež DB objektu kolekciju
+    * 
+    * @return   object	@this     collection of DB objects
+    */
     public function GetAll(){
         $data = array();
         $table = get_class($this) . 's';
@@ -132,6 +180,14 @@ class User
         return $data;
     }
     
+    /**
+    * en: Get all teble data and return object collection
+    * lv: Saņem visus tabulas datus un atgriež DB objektu kolekciju
+    * 
+    * @param    int     @id       id of current object class, PK, AI
+    * 
+    * @return   object	@this     single object of @this
+    */
     public function Get($id){
         $table = get_class($this) . 's';
         $obj = get_class($this);
@@ -151,6 +207,8 @@ class User
     
 }
 
+// en: Static functions to get an instance of class in a single connection to DB
+// lv: Statiskās funkcijas lai saglabātu DB pieslēguma instanci
 class UserContext
 {
     public static function getAllUsers($con)
