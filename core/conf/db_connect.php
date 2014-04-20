fa600a3971b3a6349655ac41fdcd294ef178d591
blteblte <martin.sky@inbox.lv>
2014-04-20 13:33:32 +0300

    ver 0.8

--- core/conf/db_connect.php
+++ core/conf/db_connect.php
@@ -1,4 +1,13 @@
 <?php
+/**
+* phpAJAX simple framework for agile AJAX development
+* 
+* @author Mārtiņš Bitenieks
+* @licence http://www.opensource.org/licenses/mit-license.html MIT License
+* @version Look at /index.php
+*/
+
+
 // en: Define your database connection configuration here
 // lv: Definē savu databāses pieslēguma konfigurāciju šeit
 
