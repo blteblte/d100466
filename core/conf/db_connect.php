212f899e2a44a5e21248985f7aeb961cc6ff89e3
blteblte <martin.sky@inbox.lv>
2014-06-04 23:35:16 +0300

    ver 0.9.1.

--- core/conf/db_connect.php
+++ core/conf/db_connect.php
@@ -11,6 +11,4 @@
 // en: Define your database connection configuration here
 // lv: Definē savu databāses pieslēguma konfigurāciju šeit
 
-return array('host'=>'localhost:3306', 'dbname'=>'martin', 'user'=>'root', 'pass'=>'');
-//return array('host'=>'mysql1.000webhost.com', 'dbname'=>'a9050695_phpajax', 'user'=>'a9050695_phpajax', 'pass'=>'mysql7524MaRtI');
-//return array('host'=>'mysql.2freehosting.com', 'dbname'=>'u773174075_phpaj', 'user'=>'u773174075_phpaj', 'pass'=>'mysql7524');
+return array('host'=>'localhost:3306', 'dbname'=>'phpajax', 'user'=>'root', 'pass'=>'');
